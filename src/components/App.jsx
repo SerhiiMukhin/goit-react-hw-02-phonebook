6a23d7f9521a298f1cad7f254ae2c88960840bbd
SerhiiMukhin <110244388+SerhiiMukhin@users.noreply.github.com>
2023-02-26 11:55:35 +0200

    step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,9 +18,11 @@ export default class App extends React.Component {
     event.preventDefault();
     const form = event.currentTarget;
     const name = form.elements.name.value;
+    const number = form.elements.number.value;
     console.log(name);
+    console.log(number);
     this.setState(({ contacts }) => ({
-      contacts: [...contacts, { id: nanoid(), name }],
+      contacts: [...contacts, { id: nanoid(), name, number }],
     }));
     form.reset();
   };
@@ -35,6 +37,13 @@ export default class App extends React.Component {
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
           <button type="submit">Add contact</button>
